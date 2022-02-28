35bb19b9c88522a4f8f212b27c7f656e673b62d0
Twigonometry <mac.j.goodwin@gmail.com>
2022-02-28 21:55:25 +0000

    Try cartouche syntax - ML error

--- Calculator.thy
+++ Calculator.thy
@@ -131,15 +131,15 @@ definition finalOutput :: "session => string" where
 
 value "finalOutput (Add 5 (Sub 4 (Div 4 (GetResult))))"
 
-export_code clear getResult add sub mul divi eval in Haskell module_name Calculator file_prefix calculator
+export_code clear getResult add sub mul divi eval in Haskell module_name Test file_prefix test
 
 
-ML {*
+ML ‹
 val gen_files = Generated_Files.get_files (Proof_Context.theory_of @{context})
 val output_dir = Path.explode "./generatedHaskellFiles/"
-*}
+›
 
-ML {* map (Generated_Files.write_file output_dir) gen_files *}
+ML ‹ map (Generated_Files.write_file output_dir) gen_files ›
 
 
 end
