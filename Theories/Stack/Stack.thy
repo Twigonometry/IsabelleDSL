25386ba74d3df31a27cb3c0dc05220c3bff9b7ba
Twigonometry <mac.j.goodwin@gmail.com>
2022-04-19 16:49:05 +0100

    Add stack theory

---
+++ Theories/Stack/Stack.thy
@@ -0,0 +1,22 @@
+theory Stack
+  imports Main
+begin
+
+datatype 'a stack = AStack "'a list" "'a list"
+
+definition empty :: "'a stack" where
+"empty = AStack [] []"
+
+primrec push :: "'a \<Rightarrow> 'a stack \<Rightarrow> 'a stack" where
+"push x (AStack xs ys) = AStack (x # xs) ys"
+
+fun pop :: "'a stack \<Rightarrow> 'a stack" where
+"pop (AStack [] []) = AStack [] []"
+| "pop (AStack (x # xs) ys) = AStack xs ys"
+| "pop (AStack [] xs) =
+(case rev xs of y # ys \<Rightarrow> AStack ys [])"
+
+fun items :: "'a stack \<Rightarrow> 'a list" where
+"items (AStack xs ys) = xs @ ys"
+
+end
