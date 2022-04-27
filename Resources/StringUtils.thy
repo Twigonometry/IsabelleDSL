1f7c020092640b90eb557a3f79dd5d8f35544c25
Twigonometry <mac.j.goodwin@gmail.com>
2022-04-27 23:57:59 +0100

    WIP second nat instance

--- Resources/StringUtils.thy
+++ Resources/StringUtils.thy
@@ -7,27 +7,31 @@ class printable =
 
 (* string functions *)
 
-fun string_of_digit :: "nat => String.literal"
-where
-  "string_of_digit n =
-    (if n = 0 then STR ''0''
-    else if n = 1 then STR ''1''
-    else if n = 2 then STR ''2''
-    else if n = 3 then STR ''3''
-    else if n = 4 then STR ''4''
-    else if n = 5 then STR ''5''
-    else if n = 6 then STR ''6''
-    else if n = 7 then STR ''7''
-    else if n = 8 then STR ''8''
-    else STR ''9'')"
+instantiation nat :: printable
+begin
+  fun string_of_digit :: "nat => String.literal"
+    where
+      "string_of_digit n =
+      (if n = 0 then STR ''0''
+      else if n = 1 then STR ''1''
+      else if n = 2 then STR ''2''
+      else if n = 3 then STR ''3''
+      else if n = 4 then STR ''4''
+      else if n = 5 then STR ''5''
+      else if n = 6 then STR ''6''
+      else if n = 7 then STR ''7''
+      else if n = 8 then STR ''8''
+      else STR ''9'')"
+instance apply standard .
+end
 
 instantiation nat :: printable
 begin
   fun string_of_nat :: "nat \<Rightarrow> String.literal"
     where
       "string_of_nat n =
-        (if n < 10 then string_of_digit n
-        else string_of_nat (n div 10) + string_of_digit (n mod 10))"
+        (if n < 10 then string_of n
+        else string_of (n div 10) + string_of (n mod 10))"
 instance apply standard .
 end
 declare string_of_nat.simps [simp del]
