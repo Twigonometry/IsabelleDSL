48617191c0ed951d1b41f6fa28d0ad03fc2d8792
Twigonometry <mac.j.goodwin@gmail.com>
2022-04-30 16:10:13 +0100

    Add Andrei changes

--- Resources/StringUtils.thy
+++ Resources/StringUtils.thy
@@ -8,18 +8,18 @@ class printable =
 (* string functions *)
 
 fun string_of_digit :: "nat => String.literal"
-where
-  "string_of_digit n =
-    (if n = 0 then STR ''0''
-    else if n = 1 then STR ''1''
-    else if n = 2 then STR ''2''
-    else if n = 3 then STR ''3''
-    else if n = 4 then STR ''4''
-    else if n = 5 then STR ''5''
-    else if n = 6 then STR ''6''
-    else if n = 7 then STR ''7''
-    else if n = 8 then STR ''8''
-    else STR ''9'')"
+    where
+      "string_of_digit n =
+      (if n = 0 then STR ''0''
+      else if n = 1 then STR ''1''
+      else if n = 2 then STR ''2''
+      else if n = 3 then STR ''3''
+      else if n = 4 then STR ''4''
+      else if n = 5 then STR ''5''
+      else if n = 6 then STR ''6''
+      else if n = 7 then STR ''7''
+      else if n = 8 then STR ''8''
+      else STR ''9'')"
 
 instantiation nat :: printable
 begin
@@ -27,10 +27,11 @@ begin
     where
       "string_of_nat n =
         (if n < 10 then string_of_digit n
-        else string_of_nat (n div 10) + string_of_digit (n mod 10))"
+        else string_of_digit (n div 10) + string_of_digit (n mod 10))"
 instance apply standard .
 end
 declare string_of_nat.simps [simp del]
+
 
 instantiation int :: printable
 begin 
