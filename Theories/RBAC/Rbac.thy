91fadb5a3aa61c6554d893ab52fc54ab4d66b4be
Twigonometry <mac.j.goodwin@gmail.com>
2022-04-30 18:52:57 +0100

    Resolve type issues

--- Theories/RBAC/Rbac.thy
+++ Theories/RBAC/Rbac.thy
@@ -3,15 +3,19 @@ imports Main
 begin
 
 datatype userID = UserID string
+datatype paperID = PaperID string
+datatype confID = ConfID string
+
+definition "emptyStr = STR ''''"
 
 (* The users of the system: *)
-datatype user = User string string string
+datatype user = User String.literal String.literal String.literal
 fun nameUser where "nameUser (User name info email) = name"
 fun infoUser where "infoUser (User name info email) = info"
 fun emailUser where "emailUser (User name info email) = email"
 definition "emptyUser \<equiv> User emptyStr emptyStr emptyStr"
 
-datatype password = Password string
+datatype password = Password String.literal
 definition "emptyPass \<equiv> Password emptyStr"
 
 (* Roles: author, reviewer (owner of the nth review of a paper), program committee (PC) member, chair *)
